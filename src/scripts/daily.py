f95ab8d7082b063d05a91028bd80e4e69764c9b4
didlly <94558954+didlly@users.noreply.github.com>
2022-01-27 17:15:32 +0000

    Fix bugs.
    Fix `daily.py` time comparison bug.

--- src/scripts/daily.py
+++ src/scripts/daily.py
@@ -5,7 +5,6 @@ from datetime import datetime
 
 def daily(log, token, channel_id, logging, cwd):
     data = load(open(f"{cwd}/data.json", "r"))
-    
     if data["daily"] == "None":
         request = post(f"https://discord.com/api/v8/channels/{channel_id}/messages", headers={"authorization": token}, data={"content": "pls daily"})
     
@@ -23,7 +22,7 @@ def daily(log, token, channel_id, logging, cwd):
         
         if logging["debug"]:
             register(log, "DEBUG", "Successfully updated latest command run of `pls daily`.")
-    elif (datetime.strptime(datetime.now().strftime("%x-%X"), "%x-%X") - datetime.strptime(data["daily"], "%x-%X")).total_seconds > 23400:
+    elif (datetime.strptime(datetime.now().strftime("%x-%X"), "%x-%X") - datetime.strptime(data["daily"], "%x-%X")).total_seconds() > 23400:
         request = post(f"https://discord.com/api/v8/channels/{channel_id}/messages", headers={"authorization": token}, data={"content": "pls daily"})
     
         if request.status_code != 200 and logging["warning"]:
